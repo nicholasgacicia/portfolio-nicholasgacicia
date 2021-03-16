d726d407a6575e760a3759c7d55b80fc653cf1b8
nicholasgacicia <nicholasgacicia@gmail.com>
2021-03-16 17:01:47 -0400

    first commit

---
+++ js/main.js
@@ -0,0 +1,31 @@
+$(document).ready(function () {
+  $('.menu-toggler').on('click', function() {
+    $(this).toggleClass('open');
+    $('.top-nav').toggleClass('open');
+  });
+
+  $('.top-nav .nav-link').on('click', function() {
+    $('.menu-toggler').removeClass('open');
+    $('.top-nav').removeClass('open');
+  });
+
+  $('nav a[href*="#"]').on('click', function() {
+    $('html, body').animate({
+      scrollTop: $($(this).attr('href')).offset().top - 40
+    }, 500);
+  });
+
+  $('#up').on('click', function() {
+    $('html, body').animate({
+      scrollTop: 0
+    }, 700);
+  });
+
+  AOS.init({
+    easing: 'ease',
+    duration: 1200,
+    once: true
+  });
+
+  
+});
